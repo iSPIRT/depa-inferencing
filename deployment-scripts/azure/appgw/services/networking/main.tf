7509b6a7f800fd0f9caf9a8862538decd2926df5
Kapil Vaswani <kapilv@microsoft.com>
2024-12-27 16:43:13 +0000

    vnet address space

--- deployment-scripts/azure/appgw/services/networking/main.tf
+++ deployment-scripts/azure/appgw/services/networking/main.tf
@@ -23,7 +23,7 @@ resource "azurerm_subnet" "subnet" {
   name                 = "subnet"
   resource_group_name  = var.resource_group_name
   virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.1.1.0/24"]
+  address_prefixes     = ["10.4.1.0/24"]
 }
 
 resource "azurerm_public_ip" "public_ip" {
