800c0c287105dbf9d8ce0919a7a41fbc6b6e2ffb
Kapil Vaswani <kapil.vaswani@gmail.com>
2025-09-17 17:47:32 +0000

    print hashes

--- policies/update_policies_tf.sh
+++ policies/update_policies_tf.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 REPO_ROOT=$(git rev-parse --show-toplevel)
+POLICIES_DIR=$REPO_ROOT/deployment-scripts/azure/offer-service/environment/demo/cce-policies
 az confcom acipolicygen -y -i kv-policy.json --print-policy | grep -v '^$' > \
-    $REPO_ROOT/deployment-scripts/azure/offer-service/environment/demo/cce-policies/kv.base64
+    $POLICIES_DIR/kv.base64
 az confcom acipolicygen -y -i ofe-policy.json --print-policy | grep -v '^$' > \
-    $REPO_ROOT/deployment-scripts/azure/offer-service/environment/demo/cce-policies/ofe.base64
+    $POLICIES_DIR/ofe.base64
 az confcom acipolicygen -y -i offer-policy.json --print-policy | grep -v '^$' > \
-    $REPO_ROOT/deployment-scripts/azure/offer-service/environment/demo/cce-policies/offer.base64
+    $POLICIES_DIR/offer.base64
+
+cat $POLICIES_DIR/kv.base64 | base64 -d | sha256sum | cut -d' ' -f1
+cat $POLICIES_DIR/ofe.base64 | base64 -d | sha256sum | cut -d' ' -f1
+cat $POLICIES_DIR/offer.base64 | base64 -d | sha256sum | cut -d' ' -f1
