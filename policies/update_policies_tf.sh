18b1996f232429e40f94a4e9bd1a5414f741ae52
Kapil Vaswani <kapil.vaswani@gmail.com>
2025-09-17 15:23:35 +0000

    using policy config

--- policies/update_policies_tf.sh
+++ policies/update_policies_tf.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 REPO_ROOT=$(git rev-parse --show-toplevel)
-az confcom acipolicygen -y -a kv.json --print-policy | grep -v '^$' > \
+az confcom acipolicygen -y -i kv-policy.json --print-policy | grep -v '^$' > \
     $REPO_ROOT/deployment-scripts/azure/offer-service/environment/demo/cce-policies/kv.base64
-az confcom acipolicygen -y -a ofe.json --print-policy | grep -v '^$' > \
+az confcom acipolicygen -y -i ofe-policy.json --print-policy | grep -v '^$' > \
     $REPO_ROOT/deployment-scripts/azure/offer-service/environment/demo/cce-policies/ofe.base64
-az confcom acipolicygen -y -a offer.json --print-policy | grep -v '^$' > \
+az confcom acipolicygen -y -i offer-policy.json --print-policy | grep -v '^$' > \
     $REPO_ROOT/deployment-scripts/azure/offer-service/environment/demo/cce-policies/offer.base64
